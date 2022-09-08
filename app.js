993876ce63f211da9cfac8da892111ac222da412
basagoitiam <marika.basagoitia@gmail.com>
2022-09-08 18:12:09 -0400

    completed exercises

--- app.js
+++ app.js
@@ -4,7 +4,33 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName = "Marika";
+let lastName = "generic last name";
+var age = 24;
 // Exercise 2
-
+lastName = "Basagoitia";
+let fullName = `${firstName} ${lastName}`;
+age = 24;
 // Exercise 3
+let language = "Javascript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+//Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+let hometown = "Monte Vista, Colorado";
+let currentCity = "New Haven, Connecticut";
+let passTime = "playing violin";
+let myStory = `Hi, my name is ${fullName}, and I am from ${hometown}. Currently, I am living in ${currentCity} where I spend my time ${passTime} and learning to code.`
+console.log(myStory);
